ca3f532c96aabf6b5fa31caf284c8ae4213e4b73
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-11 14:41:49 +0200

    cleanup

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -24,21 +24,5 @@ const NavigationAuth = () =>
   </div>
 
 const NavigationNonAuth = () => null
-  // <ul>
-  //   <li><Link to={routes.SIGN_IN}>Zaloguj</Link></li>
-  //   <li><Link to={routes.SIGN_UP}>Utwórz konto</Link></li>
-  // </ul>
-
-// const Navigation = () =>
-//   <div>
-//     <ul>
-//       <li><Link to={routes.SIGN_IN}>Zaloguj</Link></li>
-//       <li><Link to={routes.SIGN_UP}>Zarejestruj</Link></li>
-//       <li><Link to={routes.DASHBOARD}>Kokpit</Link></li>
-//       <li><Link to={routes.CHARTS}>Wykresy</Link></li>
-//       <li><Link to={routes.RATES}>Kursy</Link></li>
-//       <li><SignOutButton /></li>
-//     </ul>
-//   </div>
 
 export default Navigation;

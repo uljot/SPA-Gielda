c9cf0af5df084b5928837f7aa280c445725877e5
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-19 00:53:39 +0200

    created accounts now have wallet

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -2,11 +2,12 @@ import { db } from './firebase';
 
 // User API
 
-export const doCreateUser = (id, username, email, balance) =>
+export const doCreateUser = (id, username, email, balance, wallet) =>
   db.ref(`users/${id}`).set({
     username,
     email,
     balance,
+    wallet,
   });
 
 export const onceGetUsers = () =>

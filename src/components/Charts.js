b7a9c6bb299421212d8f9282e9c187b97ffeae94
Sudzio133 <32868975+Sudzio133@users.noreply.github.com>
2018-05-18 13:35:21 +0200

    added base chart

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import withAuthorization from './withAuthorization';
+
+import BillboardChart from "react-billboardjs";
+import "react-billboardjs/lib/billboard.css";
+
+const CHART_DATA = {
+  columns: [
+    ["data1", 30, 20, 50, 40, 60, 50],
+    ["data2", 200, 130, 90, 240, 130, 220],
+    ["data3", 300, 200, 160, 400, 250, 250]
+  ],
+  type: "line"
+};
+
+class LineChart extends Component {
+  render() {
+    return <BillboardChart data={CHART_DATA} />;
+  }
+}
 
 const Charts = () =>
   <div>
     <h1>Wykresy</h1>
-    <p>Tu będzie duży wykres z billboard.js.</p>
+    <LineChart/>
   </div>
 
   const authCondition = (authUser) => !!authUser;

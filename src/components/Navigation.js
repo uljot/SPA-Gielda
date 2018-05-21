dda7ba9ec4f0cc622f49f5f9ca3cf568f0fc1e33
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-21 07:35:33 +0200

    fix for inconsistent indentation style

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,27 +7,27 @@ import * as routes from '../constants/routes';
 import {DataAccess} from './Data';
 
 var navBar = {
-	paddingTop: "15px",
-	paddingBottom: "15px",
-	backgroundColor: "#000000",
-	textDecoration: "none",
+    paddingTop: "15px",
+    paddingBottom: "15px",
+    backgroundColor: "#000000",
+    textDecoration: "none",
 };
 
 var navRegular = {
-	padding: "0px 15px 0px 15px",
-	textDecoration: "none",
-	color: "#999999"
+    padding: "0px 15px 0px 15px",
+    textDecoration: "none",
+    color: "#999999"
 };
 
 var navActive = {
-	color: "#ffffff"
+    color: "#ffffff"
 };
 
 var rightSide = {
-	color: "#999999",
-	padding: "0px 15px 0px 15px",
-	textDecoration: "none",
-	float: "right"
+    color: "#999999",
+    padding: "0px 15px 0px 15px",
+    textDecoration: "none",
+    float: "right"
 };
 
 const Navigation = () =>
@@ -45,18 +45,18 @@ const NavigationAuth = () =>
       <NavLink activeStyle={navActive} style={navRegular} to={routes.RATES}>Kursy</NavLink>
       <NavLink style={rightSide} to={routes.GOODBYE} onClick={() => Firebase.auth().signOut()}>Wyloguj</NavLink>
       <AuthUserContext.Consumer>
-		{
-		authUser =>
-			<span>
-			  <span style={rightSide}>
-				<DataAccess from={"users/" + authUser.uid + "/username"} />
-			  </span>
-			  <span style={rightSide}>
-				Stan konta: <DataAccess from={"users/" + authUser.uid + "/balance"} /> PLN
-			  </span>
-			</span>
-		}
-	  </AuthUserContext.Consumer>
+        {
+        authUser =>
+            <span>
+              <span style={rightSide}>
+                <DataAccess from={"users/" + authUser.uid + "/username"} />
+              </span>
+              <span style={rightSide}>
+                Stan konta: <DataAccess from={"users/" + authUser.uid + "/balance"} /> PLN
+              </span>
+            </span>
+        }
+      </AuthUserContext.Consumer>
   </div>
 
 const NavigationNonAuth = () => null

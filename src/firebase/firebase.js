397c50632f65ce20836db74f49bcc18550777e42
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-24 17:21:00 +0200

    sensitive data are no longer uploaded

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,7 @@
 import * as firebase from 'firebase';
+import { details } from '../constants/ProjectDetails';
 
-const config = {
-  apiKey: "AIzaSyApFjATr-O0bA3hv-gZkYw78ko8qyoeew0",
-  authDomain: "spa-gielda.firebaseapp.com",
-  databaseURL: "https://spa-gielda.firebaseio.com",
-  projectId: "spa-gielda",
-  storageBucket: "spa-gielda.appspot.com",
-  messagingSenderId: "992853169048"
-};
+const config = details;
 
 if (!firebase.apps.length) {
   firebase.initializeApp(config);

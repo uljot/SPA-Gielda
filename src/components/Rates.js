55682f3d189d6b0d40d78a8e050ac86550633893
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-25 18:45:27 +0200

    filled part of the rates table

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import withAuthorization from './withAuthorization';
 import { Load } from './Load';
@@ -36,10 +36,48 @@ class Rates extends Load {
               </tr>
             </thead>
             <tbody>
+			  {currentRates ? lastRates ?
+                Object.keys(currentRates).map(function(key, index) {
+                  return <TableBuilder key={index}
+                                       name={currentRates[key].currency}
+                                       code={key}
+                                       mid={currentRates[key].mid}
+                                       oldMid={lastRates[key].mid}
+                                       bid={currentRates[key].bid}
+                                       ask={currentRates[key].ask}
+                         />
+                })
+            : null : null}
             </tbody>
           </table>
         </div>
       </div>
+    );
+  }
+}
+
+class TableBuilder extends Component {
+  render() {
+    const { name } = this.props;
+    const { code } = this.props;
+    const { mid } = this.props;
+    const { oldMid } = this.props;
+    const { bid } = this.props;
+    const { ask } = this.props;
+
+    return (
+      <tr>
+        <td>#</td>
+        <td>{name}</td>
+        <td>{code}</td>
+        <td>{mid.toString().replace(".",",")}</td>
+        <td>{bid ? bid.toString().replace(".",",") : "B/D"}</td>
+        <td>{ask ? ask.toString().replace(".",",") : "B/D"}</td>
+        <td>{(mid - oldMid).toLocaleString('pl-PL', { minimumFractionDigits: 2, maximumFractionDigits: 6})}({((mid / oldMid - 1) * 100).toFixed(2).replace(".",",")}%)</td>
+        <td>#</td>
+        <td>#</td>
+        <td>#</td>
+      </tr>
     );
   }
 }

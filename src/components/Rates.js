c6049fb4790f9057f59b191ce0367116caa42682
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-25 16:01:39 +0200

    rates page skeleton

--- src/components/Rates.js
+++ src/components/Rates.js
@@ -1,11 +1,48 @@
 import React from 'react';
-import withAuthorization from './withAuthorization';
 
-const Rates = () =>
-  <div>
-    <h1>Kursy</h1>
-    <p>Tu będzie strona z kursami, umożliwiająca transkacje.</p>
-  </div>
+import withAuthorization from './withAuthorization';
+import { Load } from './Load';
+
+class Rates extends Load {
+  render() {
+    const { currentRates } = this.state;
+    const { lastRates } = this.state;
+	return (
+      <div>
+        <style> {`
+          table, td, th {
+            border: 2px ridge white;
+          }
+          table {
+            width: 100%;
+          }
+        `}</style>
+        <div>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>*</th>
+                <th>Nazwa</th>
+                <th>Symbol</th>
+                <th>Kurs średni</th>
+                <th>Kurs zakupu</th>
+                <th>Kurs sprzedaży</th>
+                <th>Zmiana procentowa</th>
+                <th>Transakcja</th>
+                <th>Ilość</th>
+                <th>Kwota</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
 
   const authCondition = (authUser) => !!authUser;
 

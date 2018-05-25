75ae12670be0564b003b99611c5e34f6cfa424a8
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-25 15:46:28 +0200

    added data required by rates page to load

--- src/components/Load.js
+++ src/components/Load.js
@@ -10,6 +10,7 @@ export class Load extends Component {
     this.state = {
       user: null,
 	  currentRates: null,
+	  lastRates: null,
 	  dates: null,
 	  midsByCurrency: null
     };
@@ -22,6 +23,7 @@ export class Load extends Component {
       let user = snapshot.val().users[Firebase.auth().currentUser.uid]
 	  let dates = Object.keys(snapshot.val().rates).slice(-last)
 	  let currentRates = snapshot.val().rates[dates.slice(-1)]
+	  let lastRates = snapshot.val().rates[dates.slice(-2, -1)]
 	  let midsByCurrency = Object.keys(currentRates).map((currency) => {
 	    return dates.map((date) => {
 		  return snapshot.val().rates[date][currency].mid
@@ -30,6 +32,7 @@ export class Load extends Component {
 	  this.setState(() => ({
         user: user,
         currentRates: currentRates,
+        lastRates: lastRates,
         dates: dates,
         midsByCurrency: midsByCurrency
 	  }))

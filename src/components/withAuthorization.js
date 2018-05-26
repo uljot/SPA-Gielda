72f0a680cbcbe1eb6e963621278ce28b389d833e
Aleksander Lorenc <zedlord89@gmail.com>
2018-05-26 07:33:12 +0200

    commented repeatedly troubling lines. not signed in users are no longer redirected to sign in page.

--- src/components/withAuthorization.js
+++ src/components/withAuthorization.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import { Redirect } from 'react-router'
+/*import { Redirect } from 'react-router'*/
 
 import AuthUserContext from './AuthUserContext';
-import * as routes from '../constants/routes';
+/*import * as routes from '../constants/routes';*/
 
 const withAuthorization = (authCondition) => (Component) => {
   class WithAuthorization extends React.Component {
@@ -11,7 +11,7 @@ const withAuthorization = (authCondition) => (Component) => {
       return (
         <AuthUserContext.Consumer>
           {// eslint-disable-next-line
-		  authUser => authUser ? <Component /> : routes.noAuthRoutes.includes(window.location.pathname) ? null : <Redirect to={routes.SIGN_IN} />}
+		  authUser => authUser ? <Component /> : null/* routes.noAuthRoutes.includes(window.location.pathname) ? null : <Redirect to={routes.SIGN_IN} />*/}
         </AuthUserContext.Consumer>
       );
     }
